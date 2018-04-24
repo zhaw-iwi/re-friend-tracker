58aa586014deb927f4201e452828a5ed61b934d1
Adrian Moser <trekking09@gmail.com>
2018-04-24 15:23:43 +0200

    fix text

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker (Solution)",
+            "title": "Requirements Engineering Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",

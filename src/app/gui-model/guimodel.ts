30301bf0e24eac1e4e96da137de2068109455691
marta0336 <oehler.marta@gmail.com>
2020-12-13 20:34:55 +0000

    test

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Martas Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",

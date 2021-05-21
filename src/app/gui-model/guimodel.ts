67951487a30a5b074a38447431a77aa4432fde3b
kyburpas <pascal.kyburz@hotmail.com>
2021-05-21 15:25:50 +0000

    Hallo

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -8,7 +8,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Pascal Kyburz Kyburpas",
+            "title": "Pascal Kyburz Kybuurpas",
             "formList": [
                 {
                     "id": "FriendForm",

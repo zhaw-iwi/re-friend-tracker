ad1760e01977a66568aa8eafd1ff2022ebd49e75
Adrian Moser <mosa@zhaw.ch>
2023-01-11 19:51:24 +0100

    update

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "ZHAW Requirements Engineering Friend Tracker",
+            "title": "ZHAW Requirements Engineering Friend Tracker (FS2023)",
             "formList": [
                 {
                     "id": "FriendForm",

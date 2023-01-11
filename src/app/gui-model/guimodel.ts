f6908fb593e401f624409a94f170029e5398f2ca
Adrian Moser <mosa@zhaw.ch>
2023-01-11 19:23:32 +0100

    update

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker FS2023",
+            "title": "Requirements Engineering Friend Tracker (FS2023)",
             "formList": [
                 {
                     "id": "FriendForm",

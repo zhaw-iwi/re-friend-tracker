071437607ec3816dca2b88e60e62a3ca3012a6b3
mosazhaw <mosa@zhaw.ch>
2024-04-03 09:14:19 +0200

    fs24

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "ZHAW Requirements Engineering Friend Tracker (FS2023)",
+            "title": "ZHAW Requirements Engineering Friend Tracker (FS2024)",
             "formList": [
                 {
                     "id": "FriendForm",

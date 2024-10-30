e08150c1d1b0d5ff9fdc7e20ce973e489ddd8df5
mosazhaw <adrian.moser@zhaw.ch>
2024-10-30 09:13:11 +0000

    live reload

--- bs-config.js
+++ bs-config.js
@@ -1,9 +1,20 @@
+const url = require('url');
+
 module.exports = {
+  proxy: {
+    target: 'http://localhost:8080',
+    ws: true
+  },
   proxy: "localhost:8080",
   files: ["app/**/*.json"],
   port: 3000,
   open: true,
   notify: false,
   reloadDelay: 500,
-  reloadDebounce: 500
+  reloadDebounce: 500,
+  socket: {
+    domain: process.env.CODESPACE_NAME
+      ? `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`
+      : 'http://localhost:3000'
+  }
 };
